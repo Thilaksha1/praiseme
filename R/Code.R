f4309a60608f0a94cc7a4e60120f1a07e359f92c
Thilaksha1 <silva.thialksha@gmail.com>
2017-10-25 15:20:24 +1100

    Add some examples to praise function to

--- R/Code.R
+++ R/Code.R
@@ -1,6 +1,13 @@
 #' @title Send some praise
+#'
 #' @description Praise me at critical times in my life
+#'
 #' @export
+#'
+#' @examples
+#'
+#' # I am gonna deliver some praise!
+#'
 praise <- function(){
   print("You are the best")
 }
